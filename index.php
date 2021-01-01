35501c9e724001943bd10eba2e8b46e217d03325
P. Mergey <gzm@gizmecano.net>
2021-01-01 15:45:03 +0100

    Initialize dependencies manager

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
+<?php
+$composer = json_decode(file_get_contents('composer.json'), true);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
+  <title><?php echo $composer['description']; ?></title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <?php foreach ($composer['authors'] as $authorship) { ?>
+  <meta name="author" content="<?php echo $authorship['name']; ?>">
+  <?php } ?>
 </head>
 
 <body>
