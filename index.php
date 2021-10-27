288fda0b295a6d31c98671423ccaeb329ba1e0d6
P. Mergey <gzm@gizmecano.net>
2021-10-27 11:10:39 +0200

    Format matter heading

--- index.php
+++ index.php
@@ -58,11 +58,7 @@ $composer = json_decode(file_get_contents('composer.json'), true);
       $image = glob($folder . '/shot.{jpg,png}', GLOB_BRACE);
     ?>
       <r-cell span="4" span-s="row">
-        <h2>
-          <a href="<?php echo $input['link']; ?>">
-            <?php echo $input['name']; ?>
-          </a>
-        </h2>
+        <h2><a href="<?php echo $input['link']; ?>"><?php echo $input['name']; ?></a></h2>
         <img class="h-16 top cover" width="100%" src="<?php echo $image[0]; ?>" alt="<?php echo $input['name']; ?>">
         <?php echo gzm_text($folder . '/text.md'); ?>
       </r-cell>
